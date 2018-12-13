8147f0c5de81078687502ae167fd36f689672785
余华 <451174313@qq.com>
2018-12-13 17:55:58 +0800

    完善currentuser参数获取方法

--- src/main/java/com/newpermission/interceptor/annotation/CurrentUserAnnotationMethodArgumentResolver.java
+++ src/main/java/com/newpermission/interceptor/annotation/CurrentUserAnnotationMethodArgumentResolver.java
@@ -1,23 +1,38 @@
 package com.newpermission.interceptor.annotation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import com.newpermission.constant.ApplicationContextHelper;
+import com.newpermission.constant.CurrentUser;
+import com.newpermission.service.CommonService;
+
 public class CurrentUserAnnotationMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
+	@Autowired
+	private CommonService commonsService;
+	
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
-		return parameter.hasParameterAnnotation(CurrentUser.class);
+		return parameter.hasParameterAnnotation(CurrentUserAnnotation.class);
 	}
 
 	@Override
 	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
 			NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 		String token = webRequest.getHeader("access-token");
-		return null;
+		if (null == token) {
+			return null;
+		}
+		if (null == commonsService) {
+			commonsService = ApplicationContextHelper.getBean(CommonService.class);
+		}
+		CurrentUser cUser = commonsService.getCurrentUser(token);
+		return cUser;
 	}
 
 }
